62bfb55873086f6c80a940e1b88d1cb025e3e353
Seiya Nuta <nuta@seiya.me>
2019-07-11 01:59:02 +0900

    add build command

---
+++ lib/commands/build_command.ts
@@ -0,0 +1,23 @@
+import * as fs from "fs";
+import * as path from "path";
+import {
+    Args,
+    Command,
+    Opts,
+    BUILD_OPTS,
+} from "./command";
+import { buildApp } from "../firmware";
+import { logger } from "../logger";
+
+export class BuildCommand extends Command {
+    public static command = "build";
+    public static desc = "";
+    public static args = [];
+    public static opts = [ ...BUILD_OPTS ];
+    public static watchMode = true;
+
+    public async run(_args: Args, opts: Opts) {
+        await buildApp(opts.board, opts.appDir);
+        logger.success("Done");
+    }
+}
--- lib/commands/index.ts
+++ lib/commands/index.ts
@@ -2,9 +2,11 @@ import { NewCommand } from "./new_command";
 import { DevCommand } from "./dev_command";
 import { FlashCommand } from "./flash_command";
 import { SerialCommand } from "./serial_command";
+import { BuildCommand } from "./build_command";
 
 export const commands = [
     NewCommand,
+    BuildCommand,
     DevCommand,
     FlashCommand,
     SerialCommand,
